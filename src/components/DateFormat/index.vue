eb1d9f7ed1240539dbf736d893b76929f6910012
EdwinValeroKwak <evalero@kawak.net>
2024-06-24 07:50:06 -0500

    Cambio a composition api

--- src/components/DateFormat/index.vue
+++ src/components/DateFormat/index.vue
@@ -1,33 +1,27 @@
-<script lang="ts">
-import { defineComponent } from 'vue'
-import DateTime from './DateTime.vue' // adjust path as necessary
+<script setup lang="ts">
+import { ref } from 'vue'
+import DateTime from './DateTime.vue' // Adjust the path as necessary
 
 interface InternalDateTime {
-    year: number;
-    month: number;
-    day: number;
-  }
+  year: number
+  month: number
+  day: number
+}
 
-export default defineComponent({
-  components: {
-    DateTime,
-  },
-  data() {
-    return {
-      dateModel: { year: 2019, month: 1, day: 1 },
-    }
-  },
-  methods: {
-    serialize(date: InternalDateTime):string {
-      // Implement serialization logic if needed
-      return `${date.year}-${date.month}-${date.day}`
-    },
-    deserialize():InternalDateTime {
-      // Implement deserialization logic if needed
-      return { year: 2019, month: 1, day: 1 }
-    },
-  },
-})
+const dateModel = ref<InternalDateTime>({ year: 2019, month: 1, day: 1 })
+
+const serialize = (date: InternalDateTime): string => {
+  return `${date.year}-${date.month}-${date.day}`
+}
+
+const deserialize = (dateString: string): InternalDateTime => {
+  const parts = dateString.split('-').map((part) => parseInt(part, 10))
+  return {
+    year: parts[0],
+    month: parts[1],
+    day: parts[2],
+  }
+}
 </script>
 
 <template>
@@ -36,10 +30,6 @@ export default defineComponent({
     :serialize="serialize"
     :deserialize="deserialize"
   />
-  <pre>
-      Internal date is: {{ dateModel }}
-    </pre
-  >
 </template>
 
 <style scoped></style>
